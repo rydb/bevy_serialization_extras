9e0cc795d3086392b25b481c8fe2dbd3a66418f2
rydb <ryan.dobe@gmail.com>
2024-12-09 19:02:57 -0600

    (W.I.P) partial gltf creation from bevy mesh

--- crates/bevy_serialization_core/examples/example_world.rs
+++ crates/bevy_serialization_core/examples/example_world.rs
@@ -1,6 +1,6 @@
 //! A simple 3D scene with light shining over a cube sitting on a plane.
 
-use std::path::PathBuf;
+use std::{fs, io::Write, mem, path::PathBuf};
 
 use bevy::{prelude::*, window::PrimaryWindow};
 use bevy_egui::EguiContext;
@@ -9,9 +9,11 @@ use bevy_render::mesh::Indices;
 use bevy_serialization_core::{
     plugins::SerializationPlugin, prelude::{ComponentsOnSave, RefreshCounter, SerializationBasePlugin, ShowSerializable, ShowUnserializable, TypeRegistryOnSave}, resources::{LoadRequest, SaveRequest}
 };
+use gltf_json::validation::Checked;
 use bevy_ui_extras::UiExtrasDebug;
 use egui::{Color32, RichText, TextEdit};
 use egui_extras::{Column, TableBuilder};
+use gltf_json::validation::USize64;
 use moonshine_save::save::Save;
 use strum::IntoEnumIterator;
 use strum_macros::{Display, EnumIter};
@@ -28,6 +30,7 @@ fn main() {
             exit_condition: bevy::window::ExitCondition::OnPrimaryClosed,
             ..Default::default()
         }))
+        .register_type::<GltfTarget>()
         //.add_plugins(ObjPlugin)
         .insert_resource(UtilitySelection::default())
         .add_plugins(SerializationPlugin)
@@ -41,14 +44,58 @@ fn main() {
         .run();
 }
 
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+enum Output {
+    /// Output standard glTF.
+    Standard,
+
+    /// Output binary glTF.
+    Binary,
+}
+
+fn to_padded_byte_vector<T>(vec: Vec<T>) -> Vec<u8> {
+    let byte_length = vec.len() * mem::size_of::<T>();
+    let byte_capacity = vec.capacity() * mem::size_of::<T>();
+    let alloc = vec.into_boxed_slice();
+    let ptr = Box::<[T]>::into_raw(alloc) as *mut u8;
+    let mut new_vec = unsafe { Vec::from_raw_parts(ptr, byte_length, byte_capacity) };
+    while new_vec.len() % 4 != 0 {
+        new_vec.push(0); // pad to multiple of four bytes
+    }
+    new_vec
+}
+
+/// Calculate bounding coordinates of a list of vertices, used for the clipping distance of the model
+fn bounding_coords(points: &[Vertex]) -> ([f32; 3], [f32; 3]) {
+    let mut min = [f32::MAX, f32::MAX, f32::MAX];
+    let mut max = [f32::MIN, f32::MIN, f32::MIN];
+
+    for point in points {
+        let p = point.position;
+        for i in 0..3 {
+            min[i] = f32::min(min[i], p[i]);
+            max[i] = f32::max(max[i], p[i]);
+        }
+    }
+    (min, max)
+}
+
+#[derive(Copy, Clone, Debug)]
+#[repr(C)]
+struct Vertex {
+    position: [f32; 3],
+    color: [f32; 3],
+}
+
 pub fn serialize_as_gltf(
     mut meshes: ResMut<Assets<Mesh>>,
-    models: Query<(&Mesh3d, &Name), With<GltfTarget>>
+    models: Query<(Entity, &Mesh3d, &Name), With<GltfTarget>>,
+    mut commands: Commands,
 ) {
-    let Ok((mesh, name)) = models.get_single()
+    let Ok((e, mesh, name)) = models.get_single()
     .inspect_err(|err| warn!("test only works with 1 model at a time. Actual error: {:#?}", err))
     else {return;};
-
+    type Position = [f32; 3];
 
     let Some(mesh) = meshes.get(mesh) else {
         warn!("mesh not fetchable from handle. Exiting");
@@ -60,7 +107,8 @@ pub fn serialize_as_gltf(
         warn!("Expected positions. Exiting");
         return;
     };
-    let Some(positions) = positions.as_float3() else {
+    let ty: Option<&[Position]> = positions.as_float3();
+    let Some(triangles) = ty else {
         warn!("Expected positions ot be float3. Exiting");
         return;
     };
@@ -78,10 +126,130 @@ pub fn serialize_as_gltf(
         warn!("Expected indices. Exiting");
         return;
     };
+
+    let triangle_veritcies = triangles.to_vec().iter()
+    .map(|n| {
+        Vertex {
+            position: n.to_owned(),
+            color: [0.0, 0.0, 0.0]
+        }
+    }).collect::<Vec<_>>();
     let indices = indices.iter().map(|i| i as u32).collect::<Vec<u32>>();
+
+    let (min, max) = bounding_coords(&triangle_veritcies);
+
+
+    let output = Output::Standard;
 
     let mut root = gltf_json::Root::default();
 
+
+    //let triangle_size = size_of_val(triangles);
+    let triangle_size = mem::size_of::<Vertex>();
+    println!("triangle size: {:#}", triangle_size);
+    let buffer_length = triangles.len() * triangle_size;
+    println!("buffer length: {:#}", buffer_length);
+    let buffer = root.push(gltf_json::Buffer {  
+        byte_length: USize64::from(buffer_length),
+        extensions: Default::default(),
+        extras: Default::default(),
+        name: None,
+        uri: if output == Output::Standard {
+            Some("buffer0.bin".into())
+        } else {
+            None
+        },
+    });
+    let buffer_view = root.push(gltf_json::buffer::View {
+        buffer,
+        byte_length: USize64::from(buffer_length),
+        byte_offset: None,
+        byte_stride: Some(gltf_json::buffer::Stride(triangle_size)),
+        extensions: Default::default(),
+        extras: Default::default(),
+        name: None,
+        target: Some(Checked::Valid(gltf_json::buffer::Target::ArrayBuffer)),
+    });
+    let positions = root.push(gltf_json::Accessor {
+        buffer_view: Some(buffer_view),
+        byte_offset: Some(USize64(0)),
+        count: USize64::from(triangle_veritcies.len()),
+        component_type: Checked::Valid(gltf_json::accessor::GenericComponentType(
+            gltf_json::accessor::ComponentType::F32,
+        )),
+        extensions: Default::default(),
+        extras: Default::default(),
+        type_: Checked::Valid(gltf_json::accessor::Type::Vec3),
+        min: Some(gltf_json::Value::from(Vec::from(min))),
+        max: Some(gltf_json::Value::from(Vec::from(max))),
+        name: None,
+        normalized: false,
+        sparse: None,
+    });
+    let colors = root.push(gltf_json::Accessor {
+        buffer_view: Some(buffer_view),
+        byte_offset: Some(USize64::from(3 * mem::size_of::<f32>())),
+        count: USize64::from(triangle_veritcies.len()),
+        component_type: Checked::Valid(gltf_json::accessor::GenericComponentType(
+            gltf_json::accessor::ComponentType::F32,
+        )),
+        extensions: Default::default(),
+        extras: Default::default(),
+        type_: Checked::Valid(gltf_json::accessor::Type::Vec3),
+        min: None,
+        max: None,
+        name: None,
+        normalized: false,
+        sparse: None,
+    });
+    let primitive = gltf_json::mesh::Primitive {
+        attributes: {
+            let mut map = std::collections::BTreeMap::new();
+            map.insert(Checked::Valid(gltf_json::mesh::Semantic::Positions), positions);
+            map.insert(Checked::Valid(gltf_json::mesh::Semantic::Colors(0)), colors);
+            map
+        },
+        extensions: Default::default(),
+        extras: Default::default(),
+        indices: None,
+        material: None,
+        mode: Checked::Valid(gltf_json::mesh::Mode::Triangles),
+        targets: None,
+    };
+
+    let mesh = root.push(gltf_json::Mesh {
+        extensions: Default::default(),
+        extras: Default::default(),
+        name: None,
+        primitives: vec![primitive],
+        weights: None,
+    });
+    let node = root.push(gltf_json::Node {
+        mesh: Some(mesh),
+        ..Default::default()
+    });
+
+    root.push(gltf_json::Scene {
+        extensions: Default::default(),
+        extras: Default::default(),
+        name: None,
+        nodes: vec![node],
+    });
+    match output {
+        Output::Standard => {
+            let _ = fs::create_dir("cube");
+
+            let writer = fs::File::create("cube/cube.gltf").expect("I/O error");
+            gltf_json::serialize::to_writer_pretty(writer, &root).expect("Serialization error");
+
+            let bin = to_padded_byte_vector(triangle_veritcies.to_vec());
+            let mut writer = fs::File::create("cube/buffer0.bin").expect("I/O error");
+            writer.write_all(&bin).expect("I/O error");
+        }
+        Output::Binary => todo!(),
+    }
+    commands.entity(e).remove::<GltfTarget>();
+    // }
     // let mut serializeable_mesh = SerializeableMesh {
     //     positions: positions.to_vec(),
     //     normals: normals.to_vec(),
@@ -89,7 +257,7 @@ pub fn serialize_as_gltf(
     // };
 }
 
-#[derive(Component)]
+#[derive(Component, Reflect)]
 pub struct GltfTarget;
 /// set up a simple 3D scene
 fn setup(
@@ -105,20 +273,16 @@ fn setup(
             MeshMaterial3d(materials.add(Color::srgb(0.4, 0.5, 0.3))),
         )
     );
-    let gltf = GltfAssetLabel::Scene(0).from_asset("../../../assets/cube.glb");
-    commands.spawn(
-        SceneRoot(asset_server.load(GltfAssetLabel::Scene(0).from_asset("../../../assets/cube.glb"),))
-    );
 
     let mesh_handle: Handle<Mesh> = asset_server.load(GltfAssetLabel::Mesh(0).from_asset("../../../assets/cube.glb"));
     println!("mesh handle is {:#?}", mesh_handle);
-    // // cube
+    // // cube 
     commands.spawn(
         (
-            //Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0)).into()),
+            Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0)).into()),
             //SceneRoot(asset_server.load(GltfAssetLabel::Scene(0).from_asset("../../../assets/cube.glb"))),
 
-            Mesh3d(mesh_handle),
+            //Mesh3d(mesh_handle),
             MeshMaterial3d(materials.add(Color::Srgba(Srgba::GREEN))),
             Transform::from_xyz(0.0, 0.5, 0.0),
             Save,
