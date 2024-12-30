82416644e6295919aef9ce3084800ea2876aad87
rydb <ryan.dobe@gmail.com>
2024-12-30 17:52:03 -0600

    added demo video

--- crates/bevy_serialization_assemble/examples/robot_serialization.rs
+++ crates/bevy_serialization_assemble/examples/robot_serialization.rs
@@ -89,26 +89,26 @@ fn main() {
         .add_systems(Update, control_robot)
         // .add_systems(Update, make_robots_selectable)
         .add_systems(Update, bind_left_and_right_wheel)
-        .add_systems(Update, freeze_spawned_robots)
+        //.add_systems(Update, freeze_spawned_robots)
         .run();
 }
 
 #[derive(Component)]
 pub struct WasFrozen;
 
-//FIXME: physics bodies fly out of control when spawned, this freezes them for the user to unpause until thats fixed.
-pub fn freeze_spawned_robots(
-    mut robots: Query<
-        (Entity, &mut RigidBodyFlag),
-        (With<StructureFlag>, Without<JointFlag>, Without<WasFrozen>),
-    >,
-    mut commands: Commands,
-) {
-    for (e, mut body) in robots.iter_mut() {
-        *body = RigidBodyFlag::Fixed;
-        commands.entity(e).insert(WasFrozen);
-    }
-}
+// //FIXME: physics bodies fly out of control when spawned, this freezes them for the user to unpause until thats fixed.
+// pub fn freeze_spawned_robots(
+//     mut robots: Query<
+//         (Entity, &mut RigidBodyFlag),
+//         (With<StructureFlag>, Without<JointFlag>, Without<WasFrozen>),
+//     >,
+//     mut commands: Commands,
+// ) {
+//     for (e, mut body) in robots.iter_mut() {
+//         *body = RigidBodyFlag::Fixed;
+//         commands.entity(e).insert(WasFrozen);
+//     }
+// }
 #[derive(Component, Reflect, Display)]
 pub enum Wheel {
     Left,
