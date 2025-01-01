f94efa8974be2da2f77bc976a31097386a2fa2c7
rydb <ryan.dobe@gmail.com>
2025-01-01 12:19:00 -0600

    feature gated physics and assemble

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 pub mod prelude {
     pub use bevy_serialization_core::prelude::*;
+    #[cfg(feature = "physics")]
     pub use bevy_serialization_physics::prelude::*;
+    #[cfg(feature = "assemble")]
+    pub use bevy_serialization_assemble::prelude::*;
 }
 
 // pub use bevy_serialization_core;
